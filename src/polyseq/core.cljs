f2c50c2a806da4c0395ed8d6930fef18879f89c1
Sean <sean.t.irby@gmail.com>
2015-12-28 00:22:19 -0800

    Add start,stop, and clear sequence controls

--- src/polyseq/core.cljs
+++ src/polyseq/core.cljs
@@ -6,8 +6,6 @@
             [cljs.core.async :refer [timeout mult tap untap put! chan <! >!]]
             [clojure.string :as string]))
 
-
-
 (enable-console-print!)
 
 (defn make-beat-data [beat]
@@ -24,10 +22,13 @@
 
 (def angle-chan (chan))
 (def angle-mult (mult angle-chan))
+(def control-chan (chan))
+(def control-mult (mult control-chan))
 (def num-circles 10)
 (def circles-range (range 1 (+ 1 num-circles) 1)) ;;value of circle here is the # of beats in a circle AKA the frequency
 (defonce app-state 
-  (atom {:controls {:bpm 60}
+  (atom {:controls {:bpm 60
+                    :on true}
          :circles (mapv make-circle-data circles-range)
          :tooltip {:display false
                    :frequency nil}}))
@@ -69,16 +70,20 @@
 (defn get-sound-enveloped [wave release]
   (js/T "perc" #js {:r release} wave))
 
+
 (defn point-view [{:keys [beat frequency is-active is-playing circle-frequency circle-radius] :as data} owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:angle-chan (tap (:angle-mult (om/get-shared owner)) (chan))})
+      {:control-chan (tap (:control-mult (om/get-shared owner)) (chan))
+       :angle-chan (tap (:angle-mult (om/get-shared owner)) (chan))})
     om/IWillMount
     (will-mount [_]
       (let [on-end #(this-as this (.pause this) (om/update! data :is-playing false))
             sound (.on (get-sound-enveloped (get-sound-wave frequency) 600)  "ended" on-end)
-            angle-chan (:angle-chan (om/get-state owner))]
+            angle-chan (:angle-chan (om/get-state owner))
+            control-chan (:control-chan (om/get-state owner))]
+        ;;Listen for angle changes, could support reverse speeds with a bit more logic
         (go-loop [angle (<! angle-chan)]
           (when (and
                   (not (:is-playing @data))
@@ -86,10 +91,16 @@
                   (<= (- angle 20) (* 360 (/ beat circle-frequency)) angle))
             (.play (.bang sound))
             (om/update! data :is-playing true))
-          (recur (<! angle-chan)))))
+          (recur (<! angle-chan)))
+        ;;Listen for control message, TODO, add an identifier to single out updates
+        (go-loop [{:keys [message]} (<! control-chan)]
+          (om/update! data (merge data message))
+          (println "message received")
+          (recur (<! control-chan)))))
     om/IWillUnmount
     (will-unmount [_]
-      (untap (:angle-mult (om/get-shared owner)) (:angle-chan (om/get-state owner))))
+      (untap (:angle-mult (om/get-shared owner)) (:angle-chan (om/get-state owner)))
+      (untap (:control-mult (om/get-shared owner)) (:control-chan (om/get-state owner))))
     om/IRender
     (render [_]
       (let [tooltip (om/ref-cursor (:tooltip (om/root-cursor app-state)))
@@ -138,20 +149,24 @@
                       :style style}
           (dom/div #js {:className "hand-top"}))))))
 
-
-
 (defn controls-view [data owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div nil
-        [(dom/button #js {} "Clear Sequencer")
-         (dom/label nil "Speed:")
-         (dom/input #js {:type "range"
-                         :max 400
-                         :min 20
-                         :value (:bpm data)
-                         :onChange #(om/update! data :bpm (.. % -target -value))})]))))
+  (let [control-chan (:control-chan (om/get-shared owner))]
+    (reify
+      om/IRender
+      (render [_]
+        (dom/div #js {:className "controls-container"} nil
+          [(dom/div nil
+             (dom/label nil "Speed:")
+             (dom/input #js {:type "range"
+                             :max 400
+                             :min 20
+                             :value (:bpm data)
+                             :onChange #(om/update! data :bpm (.. % -target -value))}))
+           (dom/div nil
+             (dom/button #js {:onClick #(put! control-chan {:message {:is-active false}})} "Clear All"))
+           (dom/div nil
+             (dom/button #js {:onClick #(om/transact! data :on not)} "Start/Stop"))])))))
+
 
 
 
@@ -167,18 +182,23 @@
 (om/root circles-view
   app-state
   {:target (.getElementById js/document "app")
-   :shared {:angle-mult angle-mult}})
+   :shared {:angle-mult angle-mult
+            :control-chan control-chan
+            :control-mult control-mult}})
 
 (defonce angle-trigger ((fn []
                           (go-loop [acc 0
                                     bpm (:bpm (:controls @app-state))
                                     t (util/current-time)
                                     _ (<! (timeout 10))]
-                            (let [elapsed (/ (- (util/current-time) t) 1000)
-                                  acc' (mod (+ acc (* bpm 0.02)) 360)] ;;TODO get the actual formula for angular momentum                               
-                              (>! angle-chan acc')
-                              ;;(om/update! (om/ref-cursor (:angle (om/root-cursor app-state))) [acc'])
-                              (recur acc' (:bpm (:controls @app-state)) (util/current-time) (<! (timeout 10))))))))
+                            (if (and (get-in @app-state [:controls :on]) (.hasFocus js/document))
+                              (let [elapsed (/ (- (util/current-time) t) 1000)
+                                    acc' (mod (+ acc (* bpm 0.02)) 360)] ;;TODO get the actual formula for angular momentum                               
+                                (>! angle-chan acc')
+                                (recur acc' (:bpm (:controls @app-state)) (util/current-time) (<! (timeout 10))))
+                              (recur acc (:bpm (:controls @app-state)) (util/current-time) (<! (timeout 10))))))))
+
+
 
 
 
